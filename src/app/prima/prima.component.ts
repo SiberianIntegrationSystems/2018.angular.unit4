3812308b821a7af4d3ed3ca8e30b53e6aa8400ca
a.petrov <a.petrov@sis-it.pro>
2018-08-20 17:22:45 +0700

    misha-state

--- src/app/prima/prima.component.ts
+++ src/app/prima/prima.component.ts
@@ -7,17 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrimaComponent implements OnInit {
 
-  name: string;
-  birthDate: Date;
-  photoSource: string;
-
-  
-
-  constructor() {
-    this.name = 'Майя Плисецкая';
-    this.birthDate = new Date(1925, 10, 25);
-    this.photoSource = 'assets/img/pliseckaya.jpg';
-  }
+  constructor() { }
 
   ngOnInit() {
   }
