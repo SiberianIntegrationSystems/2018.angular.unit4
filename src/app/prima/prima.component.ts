44d684365804f8ed1f98fda5b569af6fe574650c
a.petrov <a.petrov@sis-it.pro>
2018-08-21 09:28:25 +0700

    init

--- src/app/prima/prima.component.ts
+++ src/app/prima/prima.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Prima} from "./prima";
 
 @Component({
   selector: 'app-prima',
   templateUrl: './prima.component.html',
   styleUrls: ['./prima.component.css']
 })
-export class PrimaComponent implements OnInit {
+export class PrimaComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  prima: Prima;
+
+  secondsSinceOpen: number = 0;
+
+  private timerId: number;
+
+  constructor() {
+    this.prima = new Prima(
+      'Майя Плисецкая',
+      new Date(1925, 10, 20),
+      'assets/img/pliseckaya.jpg'
+    );
+  }
 
   ngOnInit() {
+    this.timerId = setInterval(() => this.secondsSinceOpen++, 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timerId);
   }
 
 }
